3d63cb70ffb3e4d77ceb2fe243f9044fa21e63a1
Nuno Simoes <nuno@Nunos-MacBook-Pro.local>
2024-01-10 10:21:04 -0500

    Finished WTR1

--- scripts.js
+++ scripts.js
@@ -1,15 +1,3 @@
-// document.addEventListener('DOMContentLoaded', function() {
-//    var noScrollLinks = document.querySelectorAll('.no-scroll');
-
-//    noScrollLinks.forEach(function(link) {
-//        link.addEventListener('click', function(event) {
-//            event.preventDefault();
-//            event.stopPropagation(); // Stop event propagation
-//            openModal();
-//        });
-//    });
-// });
-
 function openModal() {
    var modal = document.getElementById("myModal");
    modal.style.display = "block";
